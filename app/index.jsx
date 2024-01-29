ceb0bd31ab04a61349a453696560365f27988e48
Sabrina Gooriah <sabrina.gooriah@qonto.com>
2024-01-29 22:09:09 +0100

    added babel + dist config + launched the app locally with dummy code

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+const rootElement = document.getElementById('app');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: "./app/index.js",
+    entry: "./app/index.jsx",
     module: {
         rules: [
             { test: /\.svg$/, use: "svg-inline-loader" },
             { test: /\.css$/, use: ["style-loader", "css-loader"] },
-            { test: /\.(js)$/, use: "babel-loader" },
+            { test: /\.(js|jsx)$/, use: "babel-loader" },
         ],
+      },
+      resolve: {
+        extensions: [".jsx", "..."]
       },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "index_bundle.js",
       },
     plugins: [
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+          template: "app/index.html"
+        })
     ],
     mode: process.env.NODE_ENV === 'production'
     ? 'production'
